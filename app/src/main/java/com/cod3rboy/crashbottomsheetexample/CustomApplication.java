1eba11e33ea8f9cb70b95da867c8972f38d4b171
Dheeraj Kumar <dkcalotra@hotmail.com>
2020-05-12 22:38:04 +0530

    Improved example app code

--- app/src/main/java/com/cod3rboy/crashbottomsheetexample/CustomApplication.java
+++ app/src/main/java/com/cod3rboy/crashbottomsheetexample/CustomApplication.java
@@ -19,11 +19,21 @@ package com.cod3rboy.crashbottomsheetexample;
 import android.app.Application;
 
 import com.cod3rboy.crashbottomsheet.CrashBottomSheet;
+import com.cod3rboy.crashbottomsheet.DeviceInfo;
 
 public class CustomApplication extends Application {
     public CustomApplication() {
         super();
-        CrashBottomSheet.register(this);
+        // Register without custom report action callback
+        // CrashBottomSheet.register(this);
+        // Register with custom report action callback
+        CrashBottomSheet.register(this, new CrashBottomSheet.onCrashReport() {
+            @Override
+            public void handleCrashReport(String stackTrace, DeviceInfo deviceInfo) {
+                // Write your custom action here to handle crash report e.g. send report to your server or log it in the file or whatever.
+                CrashBottomSheet.sendCrashEmail(CustomApplication.this, stackTrace, deviceInfo); // Using default email action for now.
+            }
+        });
     }
 
     @Override
